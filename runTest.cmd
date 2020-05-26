d97268dac2e3dac20ea5d3046211eec6e7e09f95
Aaron Stannard <aaron@petabridge.com>
2020-05-26 12:53:57 -0500

    added source

---
+++ runTest.cmd
@@ -0,0 +1,6 @@
+@echo off
+REM deploys all Kubernetes services
+
+dotnet build -c Release
+dotnet run -c Release --project /src/Akka14/Akka14.csproj
+dotnet run -c Release --project /src/Akka13/Akka13.csproj
---
+++ src/Akka13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Akka.Actor;
+
+namespace Akka13
+{
+    class Program
+    {
+        private static readonly string Config = @"
+        {
+           akka{
+            actor.provider = cluster
+            remote.dot-netty.tcp.hostname = ""localhost""
+            remote.dot-netty.tcp.port = 9445
+            cluster.seed-nodes = [""akka.tcp://testSys@localhost:9445,akka.tcp://testSys@localhost:9446""]
+           }
+        ";
+
+        static async Task Main(string[] args)
+        {
+            using (var actorSystem = ActorSystem.Create("testSys", Config))
+            {
+                await actorSystem.WhenTerminated;
+            }
+        }
+    }
+}
